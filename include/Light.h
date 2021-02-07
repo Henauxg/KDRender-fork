6fee5529d17334126faa3f936fb95f82ec668661
Henauxg <19689618+Henauxg@users.noreply.github.com>
2021-02-07 16:36:32 +0100

    [Light] Use ExperimEngine Timer

--- include/Light.h
+++ include/Light.h
@@ -13,38 +13,38 @@
 class Light
 {
 public:
-    enum class Type
-    {
-        CONSTANT,
-        FLICKERING
-    };
+	enum class Type
+	{
+		CONSTANT,
+		FLICKERING
+	};
 
 public:
-    Type GetType() const;
+	Type GetType() const;
 
 public:
-    Light(Type iType);
-    virtual ~Light() {}
+	Light(Type iType);
+	virtual ~Light() {}
 
 public:
-    virtual unsigned int ComputeStreamSize() const;
-    virtual void Stream(char *&ioData, unsigned int &oNbBytesWritten) const;
-    virtual void UnStream(const char *ipData, unsigned int &oNbBytesRead);
+	virtual unsigned int ComputeStreamSize() const;
+	virtual void Stream(char*& ioData, unsigned int& oNbBytesWritten) const;
+	virtual void UnStream(const char* ipData, unsigned int& oNbBytesRead);
 
 public:
-    virtual unsigned int GetValue() const = 0;
+	virtual unsigned int GetValue() const = 0;
 
 protected:
-    Type m_Type;
+	Type m_Type;
 };
 
 // Quite ugly workaround :/
-Light *UnstreamLight(const char *ipData, unsigned int &oNbBytesRead);
+Light* UnstreamLight(const char* ipData, unsigned int& oNbBytesRead);
 
 namespace LightTools
 {
-    CType GetMaxInterpolationDist(unsigned int iLightValue);
-    unsigned int GetMinLight(unsigned int iLightValue);
+	CType GetMaxInterpolationDist(unsigned int iLightValue);
+	unsigned int GetMinLight(unsigned int iLightValue);
 } // namespace LightTools
 
 // Constant light
@@ -52,19 +52,19 @@ namespace LightTools
 class ConstantLight : public Light
 {
 public:
-    ConstantLight(unsigned int iValue);
-    virtual ~ConstantLight();
+	ConstantLight(unsigned int iValue);
+	virtual ~ConstantLight();
 
 public:
-    virtual unsigned int ComputeStreamSize() const override;
-    virtual void Stream(char*& ioData, unsigned int& oNbBytesWritten) const override;
-    virtual void UnStream(const char* ipData, unsigned int& oNbBytesRead) override;
+	virtual unsigned int ComputeStreamSize() const override;
+	virtual void Stream(char*& ioData, unsigned int& oNbBytesWritten) const override;
+	virtual void UnStream(const char* ipData, unsigned int& oNbBytesRead) override;
 
 public:
-    virtual unsigned int GetValue() const override;
+	virtual unsigned int GetValue() const override;
 
 protected:
-    unsigned int m_Value;
+	unsigned int m_Value;
 };
 
 // FlickeringLight
@@ -72,25 +72,25 @@ protected:
 class FlickeringLight : public Light
 {
 public:
-    FlickeringLight(unsigned int iLow, unsigned int iHigh);
-    virtual ~FlickeringLight();
+	FlickeringLight(unsigned int iLow, unsigned int iHigh);
+	virtual ~FlickeringLight();
 
 public:
-    virtual unsigned int ComputeStreamSize() const override;
-    virtual void Stream(char*& ioData, unsigned int& oNbBytesWritten) const override;
-    virtual void UnStream(const char* ipData, unsigned int& oNbBytesRead) override;
+	virtual unsigned int ComputeStreamSize() const override;
+	virtual void Stream(char*& ioData, unsigned int& oNbBytesWritten) const override;
+	virtual void UnStream(const char* ipData, unsigned int& oNbBytesRead) override;
 
 public:
-    virtual unsigned int GetValue() const override;
+	virtual unsigned int GetValue() const override;
 
 protected:
-    unsigned int m_Low;
-    unsigned int m_High;
+	unsigned int m_Low;
+	unsigned int m_High;
 
 #ifdef __EXPERIMENGINE__
-    expengine::Timer m_Clock;
+	experim::Timer m_Clock;
 #else
-    sf::Clock m_Clock;
+	sf::Clock m_Clock;
 #endif
 };
 
--- src_common/Light.cpp
+++ src_common/Light.cpp
@@ -4,74 +4,74 @@
 
 // ****** Light *******
 
-Light::Light(Type iType):
-    m_Type(iType)
+Light::Light(Type iType) :
+	m_Type(iType)
 {
 
 }
 
 Light::Type Light::GetType() const
 {
-    return m_Type;
+	return m_Type;
 }
 
 unsigned int Light::ComputeStreamSize() const
 {
-    return sizeof(Light::Type);
+	return sizeof(Light::Type);
 }
 
-void Light::Stream(char *&ioData, unsigned int &oNbBytesWritten) const
+void Light::Stream(char*& ioData, unsigned int& oNbBytesWritten) const
 {
-    oNbBytesWritten = Light::ComputeStreamSize();
-    *(reinterpret_cast<Light::Type *>(ioData)) = m_Type;
-    ioData += oNbBytesWritten;
+	oNbBytesWritten = Light::ComputeStreamSize();
+	*(reinterpret_cast<Light::Type*>(ioData)) = m_Type;
+	ioData += oNbBytesWritten;
 }
 
-void Light::UnStream(const char *ipData, unsigned int &oNbBytesRead)
+void Light::UnStream(const char* ipData, unsigned int& oNbBytesRead)
 {
-    oNbBytesRead = Light::ComputeStreamSize();
-    m_Type = *reinterpret_cast<const Light::Type *>(ipData);
+	oNbBytesRead = Light::ComputeStreamSize();
+	m_Type = *reinterpret_cast<const Light::Type*>(ipData);
 }
 
-Light *UnstreamLight(const char *ipData, unsigned int &oNbBytesRead)
+Light* UnstreamLight(const char* ipData, unsigned int& oNbBytesRead)
 {
-    // First, look up type, DO NOT increment data pointer
-    Light::Type type = *(reinterpret_cast<const Light::Type *>(ipData));
+	// First, look up type, DO NOT increment data pointer
+	Light::Type type = *(reinterpret_cast<const Light::Type*>(ipData));
 
-    // Instantiate light and unstream it
-    oNbBytesRead = 0u;
-    Light *pRet = nullptr;
-    switch (type)
-    {
-    case Light::Type::CONSTANT:
-        pRet = new ConstantLight(0u);
-        pRet->UnStream(ipData, oNbBytesRead);
-        break;
-    case Light::Type::FLICKERING:
-        pRet = new FlickeringLight(0u, 0u);
-        pRet->UnStream(ipData, oNbBytesRead);
-        break;
-    default: // Should never be reached
-        break;
-    }
-    return pRet;
+	// Instantiate light and unstream it
+	oNbBytesRead = 0u;
+	Light* pRet = nullptr;
+	switch (type)
+	{
+	case Light::Type::CONSTANT:
+		pRet = new ConstantLight(0u);
+		pRet->UnStream(ipData, oNbBytesRead);
+		break;
+	case Light::Type::FLICKERING:
+		pRet = new FlickeringLight(0u, 0u);
+		pRet->UnStream(ipData, oNbBytesRead);
+		break;
+	default: // Should never be reached
+		break;
+	}
+	return pRet;
 }
 
 CType LightTools::GetMaxInterpolationDist(unsigned int iLightValue)
 {
-    return std::max<CType>(CType(static_cast<int>(iLightValue)) * CType(3) / POSITION_SCALE, CType(100) / POSITION_SCALE);
+	return std::max<CType>(CType(static_cast<int>(iLightValue)) * CType(3) / POSITION_SCALE, CType(100) / POSITION_SCALE);
 }
 
 unsigned int LightTools::GetMinLight(unsigned int iLightValue)
 {
-    return Clamp<unsigned int>((iLightValue * iLightValue * iLightValue) / (255u * 255u), std::min<unsigned int>(50u, iLightValue), 230u);
+	return Clamp<unsigned int>((iLightValue * iLightValue * iLightValue) / (255u * 255u), std::min<unsigned int>(50u, iLightValue), 230u);
 }
 
 // ****** ConstantLight *******
 
 ConstantLight::ConstantLight(unsigned int iValue) : Light(Light::Type::CONSTANT),
-                                                        m_Value(iValue)
-{   
+m_Value(iValue)
+{
 }
 
 ConstantLight::~ConstantLight()
@@ -81,43 +81,43 @@ ConstantLight::~ConstantLight()
 
 unsigned int ConstantLight::ComputeStreamSize() const
 {
-    return Light::ComputeStreamSize() + sizeof(unsigned int);
+	return Light::ComputeStreamSize() + sizeof(unsigned int);
 }
 
-void ConstantLight::Stream(char *&ioData, unsigned int &oNbBytesWritten) const
+void ConstantLight::Stream(char*& ioData, unsigned int& oNbBytesWritten) const
 {
-    oNbBytesWritten = 0;
-    Light::Stream(ioData, oNbBytesWritten);
+	oNbBytesWritten = 0;
+	Light::Stream(ioData, oNbBytesWritten);
 
-    *(reinterpret_cast<unsigned int *>(ioData)) = m_Value;
-    oNbBytesWritten += sizeof(unsigned int);
-    ioData += sizeof(unsigned int);
+	*(reinterpret_cast<unsigned int*>(ioData)) = m_Value;
+	oNbBytesWritten += sizeof(unsigned int);
+	ioData += sizeof(unsigned int);
 }
 
-void ConstantLight::UnStream(const char *ipData, unsigned int &oNbBytesRead)
+void ConstantLight::UnStream(const char* ipData, unsigned int& oNbBytesRead)
 {
-    oNbBytesRead = 0;
-    Light::UnStream(ipData, oNbBytesRead);
-    ipData += oNbBytesRead;
+	oNbBytesRead = 0;
+	Light::UnStream(ipData, oNbBytesRead);
+	ipData += oNbBytesRead;
 
-    oNbBytesRead += sizeof(unsigned int);
-    m_Value = *reinterpret_cast<const unsigned int *>(ipData);
+	oNbBytesRead += sizeof(unsigned int);
+	m_Value = *reinterpret_cast<const unsigned int*>(ipData);
 }
 
 unsigned int ConstantLight::GetValue() const
 {
-    return m_Value;
+	return m_Value;
 }
 
 // ****** FlickeringLight *******
 
-FlickeringLight::FlickeringLight(unsigned int iLow, unsigned int iHigh):
-    Light(Light::Type::FLICKERING),
-    m_Low(iLow),
-    m_High(iHigh)
+FlickeringLight::FlickeringLight(unsigned int iLow, unsigned int iHigh) :
+	Light(Light::Type::FLICKERING),
+	m_Low(iLow),
+	m_High(iHigh)
 {
 #ifndef __EXPERIMENGINE__
-    m_Clock.restart();
+	m_Clock.restart();
 #endif
 }
 
@@ -127,55 +127,55 @@ FlickeringLight::~FlickeringLight()
 
 unsigned int FlickeringLight::ComputeStreamSize() const
 {
-    return Light::ComputeStreamSize() + 2 * sizeof(unsigned int);
+	return Light::ComputeStreamSize() + 2 * sizeof(unsigned int);
 }
 
-void FlickeringLight::Stream(char *&ioData, unsigned int &oNbBytesWritten) const
+void FlickeringLight::Stream(char*& ioData, unsigned int& oNbBytesWritten) const
 {
-    oNbBytesWritten = 0;
-    Light::Stream(ioData, oNbBytesWritten);
+	oNbBytesWritten = 0;
+	Light::Stream(ioData, oNbBytesWritten);
 
-    *(reinterpret_cast<unsigned int *>(ioData)) = m_Low;
-    oNbBytesWritten += sizeof(unsigned int);
-    ioData += sizeof(unsigned int);
+	*(reinterpret_cast<unsigned int*>(ioData)) = m_Low;
+	oNbBytesWritten += sizeof(unsigned int);
+	ioData += sizeof(unsigned int);
 
-    *(reinterpret_cast<unsigned int *>(ioData)) = m_High;
-    oNbBytesWritten += sizeof(unsigned int);
-    ioData += sizeof(unsigned int);
+	*(reinterpret_cast<unsigned int*>(ioData)) = m_High;
+	oNbBytesWritten += sizeof(unsigned int);
+	ioData += sizeof(unsigned int);
 }
 
-void FlickeringLight::UnStream(const char *ipData, unsigned int &oNbBytesRead)
+void FlickeringLight::UnStream(const char* ipData, unsigned int& oNbBytesRead)
 {
-    oNbBytesRead = 0;
-    Light::UnStream(ipData, oNbBytesRead);
-    ipData += oNbBytesRead;
+	oNbBytesRead = 0;
+	Light::UnStream(ipData, oNbBytesRead);
+	ipData += oNbBytesRead;
 
-    oNbBytesRead += sizeof(unsigned int);
-    m_Low = *reinterpret_cast<const unsigned int *>(ipData);
-    ipData += sizeof(unsigned int);
+	oNbBytesRead += sizeof(unsigned int);
+	m_Low = *reinterpret_cast<const unsigned int*>(ipData);
+	ipData += sizeof(unsigned int);
 
-    oNbBytesRead += sizeof(unsigned int);
-    m_High = *reinterpret_cast<const unsigned int *>(ipData);
+	oNbBytesRead += sizeof(unsigned int);
+	m_High = *reinterpret_cast<const unsigned int*>(ipData);
 }
 
 unsigned int FlickeringLight::GetValue() const
 {
 #ifdef __EXPERIMENGINE__
-    int elapsedTime = static_cast<int>(m_Clock.getElapsedTime()) % 4000;
+	int elapsedTime = static_cast<int>(m_Clock.getElapsedTime<experim::Milliseconds>()) % 4000;
 #else
-    int elapsedTime = m_Clock.getElapsedTime().asMilliseconds() % 4000;
+	int elapsedTime = m_Clock.getElapsedTime().asMilliseconds() % 4000;
 #endif
 
-    unsigned int val = m_Low;
+	unsigned int val = m_Low;
 
-    if (elapsedTime <= 100)
-        val = m_High;
-    else if (1000 <= elapsedTime && elapsedTime <= 1150)
-        val = m_High;
-    else if (1500 <= elapsedTime && elapsedTime <= 1600)
-        val = m_High;
-    else if (3200 <= elapsedTime && elapsedTime <= 3250)
-        val = m_High;
+	if (elapsedTime <= 100)
+		val = m_High;
+	else if (1000 <= elapsedTime && elapsedTime <= 1150)
+		val = m_High;
+	else if (1500 <= elapsedTime && elapsedTime <= 1600)
+		val = m_High;
+	else if (3200 <= elapsedTime && elapsedTime <= 3250)
+		val = m_High;
 
-    return val;
+	return val;
 }
