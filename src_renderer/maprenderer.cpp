5ae8bd9109e5c6ec8835f52b6c8299d7fbc2cb32
Henauxg <19689618+Henauxg@users.noreply.github.com>
2021-02-07 22:28:29 +0100

    [MapRenderer] Start ExpEngine implementation

--- src_renderer/maprenderer.cpp
+++ src_renderer/maprenderer.cpp
@@ -5,6 +5,12 @@
 #include "KDTreeRenderer.h"
 #include "FP32.h"
 
+
+#ifdef __EXPERIMENGINE__
+#include <engine/utils/Timer.hpp>
+#include <engine/utils/Utils.hpp>
+#include <engine/Engine.hpp>
+#else
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/Image.hpp>
@@ -12,223 +18,276 @@
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Window/Event.hpp>
 #include <SFML/System.hpp>
+#endif
 
 #include <iostream>
 #include <fstream>
 #include <thread>
 
-int main(int argc, char **argv)
+namespace {
+	const std::string APPLICATION_NAME = "KDTree Map Renderer";
+#ifdef __EXPERIMENGINE__
+	const uint32_t APPLICATION_VERSION = EXPENGINE_MAKE_VERSION(0, 0, 1);
+#endif
+}
+
+class MapRenderer {
+
+public:
+	MapRenderer() = default;
+	int run(int argc, char** argv);
+	void tick(float deltaT);
+
+private:
+	KDTreeMap m_Map;
+	std::unique_ptr<KDTreeRenderer> m_Renderer = nullptr;
+	std::unique_ptr<Screen> m_Screen = nullptr;
+	KDRData::Vertex m_PlayerPos;
+	int m_playerDir = 0;
+	int64_t m_FrameCount = 0;
+
+#ifdef __EXPERIMENGINE__
+	std::unique_ptr<experim::Engine> m_Engine;
+#else
+	sf::Clock m_FpsClock;
+	sf::Clock m_Clock;
+#endif
+
+};
+
+int main(int argc, char** argv)
 {
-    KDTreeMap map;
-    if (argc != 3)
-    {
-        std::cout << "Usage : maprenderer -i path/to/map.kdm" << std::endl;
-        return 1;
-    }
-    else
-    {
-        char *pData = nullptr;
-        std::ifstream mapStream(argv[2], std::ios::binary | std::ios::in);
-        if(!mapStream.is_open())
-        {
-            std::cout << "Error: could not open input map" << std::endl;
-        }
+	MapRenderer mapRenderer;
+	return mapRenderer.run(argc, argv);
+}
 
-        mapStream.seekg(0, mapStream.end);
-        int length = mapStream.tellg();
-        mapStream.seekg(0, mapStream.beg);
-        pData = new char[length];
-        mapStream.read(pData, length);
+int MapRenderer::run(int argc, char** argv)
+{
+	if (argc != 3)
+	{
+		std::cout << "Usage : maprenderer -i path/to/map.kdm" << std::endl;
+		return EXIT_FAILURE;
+	}
+	else
+	{
+		char* pData = nullptr;
+		std::ifstream mapStream(argv[2], std::ios::binary | std::ios::in);
+		if (!mapStream.is_open())
+		{
+			std::cout << "Error: could not open input map" << std::endl;
+		}
 
-        if(pData)
-        {
-            unsigned int dummy;
-            map.UnStream(pData, dummy);
+		mapStream.seekg(0, mapStream.end);
+		int length = mapStream.tellg();
+		mapStream.seekg(0, mapStream.beg);
+		pData = new char[length];
+		mapStream.read(pData, length);
 
-            delete pData;
-            pData = nullptr;
-        }
-    }
+		if (pData)
+		{
+			unsigned int dummy;
+			m_Map.UnStream(pData, dummy);
 
-    KDTreeRenderer renderer(map);
+			delete pData;
+			pData = nullptr;
+		}
+	}
 
-    KDRData::Vertex playerPos;
-    playerPos.m_X = map.GetPlayerStartX();
-    playerPos.m_Y = map.GetPlayerStartY();
+	m_Renderer = std::make_unique<KDTreeRenderer>(m_Map);
+	m_Screen = std::make_unique<Screen>(*m_Renderer);
 
-    int playerDir = map.GetPlayerStartDirection();
+	m_PlayerPos.m_X = m_Map.GetPlayerStartX();
+	m_PlayerPos.m_Y = m_Map.GetPlayerStartY();
+	m_playerDir = m_Map.GetPlayerStartDirection();
 
-    renderer.SetPlayerCoordinates(playerPos, playerDir);
+	m_Renderer->SetPlayerCoordinates(m_PlayerPos, m_playerDir);
 
-    CType dr(180.f / POSITION_SCALE);
-    int dtheta = 140 << ANGLE_SHIFT;
+#ifdef __EXPERIMENGINE__
+	m_Engine = std::make_unique<experim::Engine>(APPLICATION_NAME, APPLICATION_VERSION);
+	m_Engine->onTick(this);
+	experim::Timer totalClock;
 
-    sf::RenderWindow app(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT),
-                         "KDTree Map Renderer",
-                         sf::Style::Close);
+	// Main loop
+	m_Engine->run();
 
-    Screen screen(renderer);
-    sf::Clock clock;
+	double totalElapsedMs = totalClock.getElapsedTime<experim::Milliseconds>();
+#else
+	sf::RenderWindow app(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT),
+		APPLICATION_NAME,
+		sf::Style::Close);
 
-    int directionFront = 0;
-    int directionBack = 0;
-    int directionStrafeLeft = 0;
-    int directionStrafeRight = 0;
-    int directionLeft = 0;
-    int directionRight = 0;
-    int slowDown = 0;
+	sf::Clock totalClock;
+	totalClock.restart();
 
-    unsigned int poolEvent = 0;
-    unsigned int showFPS = 0;
-    sf::Clock fpsClock;
-    sf::Clock totalClock;
-    totalClock.restart();
+	// Main loop
+	while (app.isOpen())
+	{
+		tick();
+	}
 
-    int64_t frameCount = 0;
-    while (app.isOpen())
-    {
-        sf::Event event;
+	double totalElapsedMs = totalClock.getElapsedTime().asMilliseconds();
+#endif
 
-        screen.refresh();
-        app.draw(screen);
-        app.display();
+	// Shitty average, should be weighted by time and not framecount
+	double averageFps = static_cast<double>(m_FrameCount) / totalElapsedMs * 1000.0;
+	std::cout << "Average FPS = " << averageFps << std::endl;
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::O))
-        {
-            directionFront = 1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::L))
-        {
-            directionBack = -1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::K))
-        {
-            directionRight = -1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::M))
-        {
-            directionLeft = 1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
-        {
-            directionStrafeRight = -1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-        {
-            directionStrafeLeft = 1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
-        {
-            slowDown = 8;
-        }
+	return EXIT_SUCCESS;
+}
 
-        if (!sf::Keyboard::isKeyPressed(sf::Keyboard::O))
-        {
-            directionFront = 0;
-        }
-        if (!sf::Keyboard::isKeyPressed(sf::Keyboard::L))
-        {
-            directionBack = 0;
-        }
-        if (!sf::Keyboard::isKeyPressed(sf::Keyboard::K))
-        {
-            directionRight = 0;
-        }
-        if (!sf::Keyboard::isKeyPressed(sf::Keyboard::M))
-        {
-            directionLeft = 0;
-        }
-        if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
-        {
-            directionStrafeRight = 0;
-        }
-        if (!sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-        {
-            directionStrafeLeft = 0;
-        }
-        if (!sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
-        {
-            slowDown = 0;
-        }
+void MapRenderer::tick(float deltaT)
+{
+	static unsigned int showFPS = 0;
+	static const CType dr(180.f / POSITION_SCALE);
+	static const int dtheta = 140 << ANGLE_SHIFT;
 
-        // if(poolEvent++ % 20 == 0)
-        {
-            while (app.pollEvent(event))
-            {
-                switch (event.type)
-                {
-                case sf::Event::Closed:
-                    app.close();
-                    break;
-                case sf::Event::KeyPressed:
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-                    {
-                        std::cout << "Dumping player info:" << std::endl;
-                        std::cout << "x = " << playerPos.m_X * POSITION_SCALE << std::endl;
-                        std::cout << "y = " << playerPos.m_Y * POSITION_SCALE << std::endl;
-                        std::cout << "dir = " << playerDir << " (= " << static_cast<float>(playerDir) / (1 << ANGLE_SHIFT) << " degres)" << std::endl;
-                    }
-                    break;
-                default:
-                    break;
-                }
-            }
-        }
+	int directionFront = 0;
+	int directionBack = 0;
+	int directionStrafeLeft = 0;
+	int directionStrafeRight = 0;
+	int directionLeft = 0;
+	int directionRight = 0;
+	int slowDown = 0;
+	unsigned int poolEvent = 0;
 
-        float deltaT = (float)(clock.getElapsedTime().asMilliseconds());
-        clock.restart();
+	sf::Event event;
 
-        // 60 FPS cap
-        // if (deltaT < (1000.f / 60.f))
-        // {
-        //     std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(1000.f / 60.f - deltaT)));
-        //     deltaT += (float)(clock.getElapsedTime().asMilliseconds());
-        // }
+	m_Screen->refresh();
+	app.draw(m_Screen);
+	app.display();
 
-        // if (1000.f / deltaT < 60.f)
-        const int fpsStep = 100;
-        if (showFPS++ % fpsStep == 0)
-        {
-            float deltaTFps = (float)(fpsClock.getElapsedTime().asMilliseconds());
-            std::cout << "FPS = " << (1000.f * static_cast<float>(fpsStep)) / deltaTFps << std::endl;
-            fpsClock.restart();
-        }
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::O))
+	{
+		directionFront = 1;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::L))
+	{
+		directionBack = -1;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::K))
+	{
+		directionRight = -1;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::M))
+	{
+		directionLeft = 1;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+	{
+		directionStrafeRight = -1;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		directionStrafeLeft = 1;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+	{
+		slowDown = 8;
+	}
 
-        frameCount++;
+	if (!sf::Keyboard::isKeyPressed(sf::Keyboard::O))
+	{
+		directionFront = 0;
+	}
+	if (!sf::Keyboard::isKeyPressed(sf::Keyboard::L))
+	{
+		directionBack = 0;
+	}
+	if (!sf::Keyboard::isKeyPressed(sf::Keyboard::K))
+	{
+		directionRight = 0;
+	}
+	if (!sf::Keyboard::isKeyPressed(sf::Keyboard::M))
+	{
+		directionLeft = 0;
+	}
+	if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+	{
+		directionStrafeRight = 0;
+	}
+	if (!sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		directionStrafeLeft = 0;
+	}
+	if (!sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+	{
+		slowDown = 0;
+	}
 
-        CType dPos = ((directionFront + directionBack) * dr * static_cast<CType>(deltaT)) / 1000;
-        CType dPosOrtho = ((directionStrafeLeft + directionStrafeRight) * dr * static_cast<CType>(deltaT)) / 1000;
-        CType dDir = ((directionLeft + directionRight) * dtheta * (deltaT)) / 1000;
+	// if(poolEvent++ % 20 == 0)
+	{
+		while (app.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				app.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+				{
+					std::cout << "Dumping player info:" << std::endl;
+					std::cout << "x = " << m_PlayerPos.m_X * POSITION_SCALE << std::endl;
+					std::cout << "y = " << m_PlayerPos.m_Y * POSITION_SCALE << std::endl;
+					std::cout << "dir = " << m_playerDir << " (= " << static_cast<float>(m_playerDir) / (1 << ANGLE_SHIFT) << " degres)" << std::endl;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	}
 
-        dPos = dPos / (slowDown + 1);
-        dDir = dDir / (slowDown + 1);
+#ifndef __EXPERIMENGINE__
+	float deltaT = (float)(m_Clock.getElapsedTime().asMilliseconds());
+	m_Clock.restart();
+#endif
 
-        // if(dPos > 0)
-        //     std::cout << "dPos = " << dPos << std::endl;
+#if 0
+	// 60 FPS cap
+	if (deltaT < (1000.f / 60.f))
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(1000.f / 60.f - deltaT)));
+		deltaT += (float)(m_Clock.getElapsedTime().asMilliseconds());
+	}
+#endif
 
-        KDRData::Vertex look(renderer.GetLook());
+#ifndef __EXPERIMENGINE__
+	// if (1000.f / deltaT < 60.f)
+	const int fpsStep = 100;
+	if (showFPS++ % fpsStep == 0)
+	{
+		float deltaTFps = (float)(m_FpsClock.getElapsedTime().asMilliseconds());
+		std::cout << "FPS = " << (1000.f * static_cast<float>(fpsStep)) / deltaTFps << std::endl;
+		m_FpsClock.restart();
+	}
+#endif
 
-        CType dx = dPos * (look.m_X - playerPos.m_X);
-        CType dy = dPos * (look.m_Y - playerPos.m_Y);
+	CType dPos = ((directionFront + directionBack) * dr * static_cast<CType>(deltaT)) / 1000;
+	CType dPosOrtho = ((directionStrafeLeft + directionStrafeRight) * dr * static_cast<CType>(deltaT)) / 1000;
+	CType dDir = ((directionLeft + directionRight) * dtheta * (deltaT)) / 1000;
 
-        playerPos.m_X += dx;
-        playerPos.m_Y += dy;
+	dPos = dPos / (slowDown + 1);
+	dDir = dDir / (slowDown + 1);
 
-        playerDir = playerDir + dDir;
-        while (playerDir < 0)
-            playerDir += 360 << ANGLE_SHIFT;
-        while (playerDir > (360 << ANGLE_SHIFT))
-            playerDir -= 360 << ANGLE_SHIFT;
+	KDRData::Vertex look(m_Renderer->GetLook());
 
-        renderer.SetPlayerCoordinates(playerPos, playerDir);
+	CType dx = dPos * (look.m_X - m_PlayerPos.m_X);
+	CType dy = dPos * (look.m_Y - m_PlayerPos.m_Y);
 
-        renderer.ClearBuffers();
-        renderer.RefreshFrameBuffer();
-    }
+	m_PlayerPos.m_X += dx;
+	m_PlayerPos.m_Y += dy;
 
-    // Shitty average, should be weighted by time and not framecount
-    double averageFps = static_cast<double>(frameCount) / (double)(totalClock.getElapsedTime().asMilliseconds()) *1000.0;
-    std::cout << "Average FPS = " << averageFps << std::endl;
+	m_playerDir = m_playerDir + dDir;
+	while (m_playerDir < 0)
+		m_playerDir += 360 << ANGLE_SHIFT;
+	while (m_playerDir > (360 << ANGLE_SHIFT))
+		m_playerDir -= 360 << ANGLE_SHIFT;
 
-    return 0;
+	m_Renderer->SetPlayerCoordinates(m_PlayerPos, m_playerDir);
+
+	m_Renderer->ClearBuffers();
+	m_Renderer->RefreshFrameBuffer();
+
+	m_FrameCount++;
 }
